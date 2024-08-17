dae02350556247ff087a3c2726a1fce81b531c42
Sunny Kumar <sunnyraj3600702@gmail.com>
2024-08-17 10:52:52 +0530

    tic-tac-toe project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Grid from './components/Grid/Grid';
+
+function App() {
+  
+
+  return (
+    <>
+        <Grid numberOfCards={9}/>
+    </>
+  )
+}
+
+export default App
